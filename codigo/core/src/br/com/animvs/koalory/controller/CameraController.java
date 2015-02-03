635d3ab101282d535c5435ef475777ea0b29adea
Andrei Daldegan <andrei.daldegan@gmail.com>
2015-02-03 05:00:00 -0200

    ADD: Posição da câmera limitada

--- codigo/core/src/br/com/animvs/koalory/controller/CameraController.java
+++ codigo/core/src/br/com/animvs/koalory/controller/CameraController.java
@@ -74,24 +74,24 @@ public final class CameraController extends BaseController {
             camera.zoom = MathUtils.lerp(camera.zoom, desiredZoom, Gdx.graphics.getDeltaTime() * Configurations.CORE_CAMERA_SPEED_MULTIPLIER);
         }
 
-        float minX = ((Configurations.RESOLUTION_REAL.x / 2f) * camera.zoom) * (Configurations.GAMEPLAY_ZOOM_MAX - camera.zoom);
-        float minY = ((Configurations.RESOLUTION_REAL.y / 2f) * camera.zoom) * (Configurations.GAMEPLAY_ZOOM_MAX - camera.zoom);
+        float minX = ((Configurations.RESOLUTION_REAL.x / 2f * camera.zoom) * camera.zoom) * (Configurations.GAMEPLAY_ZOOM_MAX - camera.zoom);
+        float minY = (Configurations.RESOLUTION_REAL.y / 2f) * camera.zoom;
 
-        if (camera.position.x < minX || camera.position.y < minX) {
+        if (camera.position.x < minX || camera.position.y < minY) {
             if (camera.position.x < minX) {
                 camera.position.x = minX;
                 positionCache.set(minX, positionCache.y);
                 desiredPosition.set(minX, positionCache.y);
             }
 
-            /*if (camera.position.y < minY) {
+            if (camera.position.y < minY) {
                 camera.position.y = minY;
                 positionCache.set(positionCache.x, minY);
-            }*/
-
-            Gdx.app.log("ASD", "X: " + camera.position.x + " Y: " + camera.position.y);
+                desiredPosition.set(positionCache.x, minY);
+            }
 
             //camera.position.y = Configurations.RESOLUTION_REAL.y / 2f;
+            //Gdx.app.log("CAMERA DEBUGGER", "X: " + camera.position.x + "Y: " + camera.position.y);
             needUpdate = true;
         }
 
@@ -163,8 +163,8 @@ public final class CameraController extends BaseController {
         float zoomNeededX = 1f;
         float zoomNeededY = 1f;
 
-        zoomNeededX = coverageX / Configurations.RESOLUTION_REAL.x * 3f;
-        zoomNeededY = coverageY / Configurations.RESOLUTION_REAL.y * 3f;
+        zoomNeededX = coverageX / (Configurations.RESOLUTION_REAL.x * 0.5f);
+        zoomNeededY = coverageY / (Configurations.RESOLUTION_REAL.y * 0.5f);
 
         if (zoomNeededX > zoomNeededY)
             desiredZoom = zoomNeededX;
